59249010a398d83a43cbbcd7e0c6a7629dde6f6d
TommyO <tom@ussie.rocks>
2020-05-17 02:25:21 -0400

    Adding android starting example

---
+++ examples/PentodeKotlin/app/src/main/java/com/example/pentodekotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pentodekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.loadUrl("https://example.com")
+    }
+}
